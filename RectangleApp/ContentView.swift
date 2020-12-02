6f46754ad3a2189a81d5ba2c2d56bdd86ddca37f
kawaaa26 <unic.almj@yahoo.com>
2020-12-03 03:27:42 +0900

    :tada: initial commitment for RectangleApp

---
+++ RectangleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RectangleApp
+//
+//  Created by KazukiSaito on 2020/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RectangleApp/RectangleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleAppApp.swift
+//  RectangleApp
+//
+//  Created by KazukiSaito on 2020/12/03.
+//
+
+import SwiftUI
+
+@main
+struct RectangleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
